9764bf1d089ed53a3ee8d6290fa07e1fe0804471
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-05-24 09:20:28 +0900

    Service: Usage

---
+++ usage/find_service.go
@@ -0,0 +1,48 @@
+// Copyright 2022 The webaccel-service-go authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package usage
+
+import (
+	"context"
+
+	"github.com/sacloud/services/helper"
+	"github.com/sacloud/webaccel-api-go"
+)
+
+func (s *Service) Find(req *FindRequest) ([]*Monthly, error) {
+	return s.FindWithContext(context.Background(), req)
+}
+
+func (s *Service) FindWithContext(ctx context.Context, req *FindRequest) ([]*Monthly, error) {
+	if err := helper.ValidateStruct(s, req); err != nil {
+		return nil, err
+	}
+
+	client := webaccel.NewOp(s.client)
+	found, err := client.MonthlyUsage(ctx, req.targetYM())
+	if err != nil {
+		return nil, err
+	}
+
+	var results []*Monthly
+	for i := range found.MonthlyUsages {
+		results = append(results, &Monthly{
+			Year:         found.Year,
+			Month:        found.Month,
+			MonthlyUsage: *found.MonthlyUsages[i],
+		})
+	}
+	return results, nil
+}
---
+++ usage/service_test.go
@@ -0,0 +1,55 @@
+// Copyright 2022 The webaccel-service-go authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package usage
+
+import (
+	"testing"
+	"time"
+
+	client "github.com/sacloud/api-client-go"
+	"github.com/sacloud/packages-go/testutil"
+	"github.com/sacloud/webaccel-api-go"
+	"github.com/stretchr/testify/require"
+)
+
+var caller = &webaccel.Client{Options: &client.Options{UserAgent: "webaccel-service-go/v" + webaccel.Version}}
+
+// TestService_CRUD_plus_L CRUD+L
+//
+// Note: 実行時にサイトが1件以上登録済みであること
+func TestService_CRUD_plus_L(t *testing.T) {
+	if !testutil.IsAccTest() {
+		t.Skip("environment variables required: TESTACC")
+	}
+	testutil.PreCheckEnvsFunc(
+		"SAKURACLOUD_ACCESS_TOKEN",
+		"SAKURACLOUD_ACCESS_TOKEN_SECRET",
+	)(t)
+	svc := New(caller)
+
+	t.Run("List", func(t *testing.T) {
+		found, err := svc.Find(&FindRequest{})
+
+		require.NoError(t, err)
+		require.NotEmpty(t, found)
+		if len(found) > 0 {
+			require.NotEmpty(t, found[0].MonthlyUsage)
+
+			now := time.Now() // yearとmonthを省略した場合は当月となるはず
+			require.Equal(t, now.Year(), found[0].Year)
+			require.Equal(t, now.Month(), time.Month(found[0].Month))
+		}
+	})
+}
