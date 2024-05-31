498c605d037d56abcb2026e200b3f72269755f90
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2024-05-31 13:14:22 +0900

    ACL機能

---
+++ site/acl/create_service.go
@@ -0,0 +1,34 @@
+// Copyright 2022-2023 The sacloud/webaccel-service-go authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package acl
+
+import (
+	"context"
+
+	"github.com/sacloud/services/helper"
+	"github.com/sacloud/webaccel-api-go"
+)
+
+func (s *Service) Create(req *CreateRequest) (*webaccel.ACLResult, error) {
+	return s.CreateWithContext(context.Background(), req)
+}
+
+func (s *Service) CreateWithContext(ctx context.Context, req *CreateRequest) (*webaccel.ACLResult, error) {
+	if err := helper.ValidateStruct(s, req); err != nil {
+		return nil, err
+	}
+	client := webaccel.NewOp(s.client)
+	return client.UpsertACL(ctx, req.SiteId, req.ACL)
+}
